b90b48fff38eed72fbcd73e03e9d3fd145624f07
Andrew Miller-Klugman <126700713+amillerklugman@users.noreply.github.com>
2024-11-04 13:27:49 -0500

    Update GPU_colabfold.sh

--- Working_directory/GPU_colabfold.sh
+++ Working_directory/GPU_colabfold.sh
@@ -17,19 +17,19 @@ module load gcc/9.2.0
 module list
 
 # Initialize Conda 
-source /work/daylab/andrew/millerklugman.a/miniconda3/etc/profile.d/conda.sh
+source <path/to/conda/install/sh>
 
 # Activate ColabFold environment
 conda activate colabfold
 
 # 1. Run the SLURM script to process the dimers with ColabFold
 echo "Running ColabFold on the generated dimers..."
-bash /work/daylab/andrew/millerklugman.a/opt/localcolabfold/outputdir//colabfold_structure.sh
+bash <path/to/colabfold_structure.sh>
 echo "ColabFold processing complete."
 
 # 2. Move the relaxed PDB files to the PARP3_dimer_model_relax directory
 echo "Moving relaxed PDB files to the PARP3_dimer_model_relax directory..."
-python /work/daylab/andrew/millerklugman.a/opt/localcolabfold/outputdir//move_relax_model.py
+python <path/to/move_relax_model.py>
 echo "Relaxed PDB files moved."
 
 # 3. Deactivate the Conda environment and change to PPIScreen
@@ -37,19 +37,19 @@ echo "Deactivating ColabFold environment..."
 conda deactivate
 echo "Switching to PPIScreen environment..."
 conda activate ppiscreen
-export PATH=/home/millerklugman.a/miniconda3/envs/ppiscreen/bin:$PATH
+export PATH=<path/to/miniconda3/envs/ppiscreen/bin:$PATH>
 
 # 4. Switch to the PPIScreenML directory
 echo "Switching to PPIScreenML directory..."
-cd /work/daylab/andrew/millerklugman.a/opt/PPIScreenML/PPIscreenML
+cd <path/to/PPIscreenML>
 echo "Switched to PPIScreenML directory."
 
 # 5. Run the PPIScreenML tool
 echo "Running PPIScreenML on the generated dimers..."
-python /work/daylab/andrew/millerklugman.a/opt/PPIScreenML/PPIscreenML/get_classification.py \
-    --working_directory /work/daylab/andrew/millerklugman.a/opt/localcolabfold/outputdir//PARP3_dimer_model_relax \
+python <path/to/PPIscreenML/get_classification.py> \
+    --working_directory <path/to/target_dimer_model_relax> \
     --protein1_chains_input A \
     --protein2_chains_input B \
-    --csv_name interaction_all_proteins_vs_parp3.csv 
+    --csv_name interaction_all_proteins_vs_bait.csv 
 echo "PPIScreenML processing complete."
 echo "All jobs completed."
