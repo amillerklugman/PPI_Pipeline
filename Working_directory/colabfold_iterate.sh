1275ab13f4b512b959d1daafc44a95cd3af1b9b5
Andrew Miller-Klugman <126700713+amillerklugman@users.noreply.github.com>
2024-11-04 13:30:13 -0500

    Update colabfold_iterate.sh

--- Working_directory/colabfold_iterate.sh
+++ Working_directory/colabfold_iterate.sh
@@ -1,19 +1,19 @@
 
 # Initialize Conda 
-source /work/daylab/andrew/millerklugman.a/miniconda3/etc/profile.d/conda.sh
+source <path/to/miniconda3/etc/profile.d/conda.sh>
 
 # Activate ColabFold environment
 conda activate colabfold
 
 # Explicitly set the Conda environment Python to be used
-export PATH=/work/daylab/andrew/millerklugman.a/miniconda3/envs/colabfold/bin:$PATH
+export PATH=<path/to/miniconda3/envs/colabfold/bin:$PATH>
 
 # Add the correct directory where hhsearch is located to PATH
-export PATH=/work/daylab/andrew/millerklugman.a/opt/hh-suite-3.2.0/build/src/:$PATH
+export PATH=<path/to/hh-suite-3.2.0/build/src/:$PATH>
 
 # Define directories for dimer FASTA files and output
-dimer_fasta_directory="/work/daylab/andrew/millerklugman.a/opt/localcolabfold/outputdir//PARP3_dimers"  # Dimer FASTA directory
-colabfold_output_directory="/work/daylab/andrew/millerklugman.a/opt/localcolabfold/outputdir//PARP3_dimer_models"  # Output directory for ColabFold
+dimer_fasta_directory="<path/to/target_dimers>"  # Dimer FASTA directory
+colabfold_output_directory="<path/to/target_dimer_models>"  # Output directory for ColabFold
 
 # Iterate over each dimer FASTA file and run colabfold_batch
 for dimer_fasta in "$dimer_fasta_directory"/*.fasta; do
@@ -21,7 +21,7 @@ for dimer_fasta in "$dimer_fasta_directory"/*.fasta; do
         echo "Running ColabFold on $dimer_fasta"
         
         # Run localcolabfold with appropriate arguments for each dimer FASTA
-        /work/daylab/andrew/millerklugman.a/opt/localcolabfold/colabfold-conda/bin/colabfold_batch --templates --amber \
+        <path/to/localcolabfold/colabfold-conda/bin/colabfold_batch> --templates --amber \
             --num-relax 2 --num-recycle 5 --use-gpu-relax "$dimer_fasta" "$colabfold_output_directory" \
             --max-msa 512:1024
         
